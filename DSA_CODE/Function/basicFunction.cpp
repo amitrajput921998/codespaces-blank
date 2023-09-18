557d332fc6b17f37aad1bdd87bb9e87f08f995e4
Amit Singh <49475880+amitrajput921998@users.noreply.github.com>
2023-09-18 14:19:21 +0000

    first commit

---
+++ DSA_CODE/Function/basicFunction.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+void printMessage(){
+    for(int i=0;i<5;i++){
+        cout<<"Hello"<<endl;
+    }
+}
+
+int main(){
+    printMessage();
+    return 0;
+}
